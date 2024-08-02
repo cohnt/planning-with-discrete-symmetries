7c38a306ac05239d3aea0ea9a963e55a77a5de73
cohnt <cohnt@umich.edu>
2024-08-02 17:50:25 -0400

    Numerically compute the affine hull to estimate the dimension.

--- src/quotient_embedding.py
+++ src/quotient_embedding.py
@@ -1,7 +1,10 @@
 import numpy as np
 import functools
 import itertools
+from scipy.stats import special_ortho_group
 from tqdm import tqdm
+
+from pydrake.all import VPolytope, AffineSubspace
 
 def tensordot(vecs):
 	return functools.reduce(lambda u, v: np.tensordot(u, v, axes=0), vecs)
@@ -24,7 +27,7 @@ def symmetrized_tensor_identity(alpha):
 	return M_alpha
 
 class Embedding:
-	def __init__(self, alpha, u, S, beta=None):
+	def __init__(self, alpha, u, S, beta=None, dimension_upper_bound=100):
 		self.n = len(alpha)
 		self.alpha = alpha
 		self.u = u
@@ -39,8 +42,13 @@ class Embedding:
 			assert len(u_i) == 3
 		assert len(self.beta) == self.n
 
-		self.M_alpha = self.compute_M_alpha()
-		self.scaled_M_alpha = np.hstack([M_alpha_i / (alpha_i + 1) for M_alpha_i, alpha_i in zip(self.M_alpha, self.alpha)])
+		# self.M_alpha = self.compute_M_alpha()
+		# self.scaled_M_alpha = np.hstack([M_alpha_i / (alpha_i + 1) for M_alpha_i, alpha_i in zip(self.M_alpha, self.alpha)])
+
+		self.dimension_upper_bound = dimension_upper_bound
+
+		self.affine_hull = self.compute_affine_hull()
+		print("Affine hull dimension", self.affine_hull.AffineDimension())
 
 	def compute_M_alpha(self):
 		M_alpha = []
@@ -51,16 +59,16 @@ class Embedding:
 				M_alpha.append(np.zeros(3 ** alpha_i))
 		return M_alpha
 
-		# M_alpha_entries = []
-		# for i in range(len(self.alpha)):
-		# 	alpha_i = self.alpha[i]
-		# 	if (alpha_i + 1) % 2 == 0:
-		# 		mult = np.math.factorial(alpha_i+1)
-		# 		M_alpha_i = np.zeros(np.power(tuple([3] * alpha_i + 1)))
-		# 		M_alpha_entries.append(symmetrize(tensordot([np.eye(3)] * int((alpha_i + 1) / 2))))
-		# 	else:
-		# 		M_alpha_entries.append(tensordot([np.zeros(3)] * alpha_i).flatten())
-		# return tensordot(M_alpha_entries).flatten()
+	def compute_affine_hull(self):
+		np.random.seed(0)
+		tol = 1e-12
+
+		# Generate a number of random SO(3) matrices, pass them through the
+		# mapping, and compute the affine hull.
+		xs = special_ortho_group.rvs(3, self.dimension_upper_bound)
+		ys = np.array([self(x) for x in xs]).T
+		vpoly = VPolytope(ys)
+		return AffineSubspace(vpoly, tol)
 
 	def E_alpha_u(self, R):
 		return np.hstack([
@@ -71,7 +79,7 @@ class Embedding:
 	def E_alpha_u_S(self, R):
 		return np.sum([
 			self.E_alpha_u(R @ S_i) for S_i in self.S.matrices
-		]) / self.S.order()
+		], axis=0) / self.S.order()
 
 	def E_alpha_beta_u(self, R):
 		return np.hstack([
@@ -84,14 +92,14 @@ class Embedding:
 			self.E_alpha_beta_u(R @ S_i) for S_i in self.S.matrices
 		], axis=0) / self.S.order()
 
-	def tilde_E_alpha_u_S(self, R):
-		return self.E_alpha_u_S(R) - self.scaled_M_alpha
+	# def tilde_E_alpha_u_S(self, R):
+	# 	return self.E_alpha_u_S(R) - self.scaled_M_alpha
 
-	def tilde_E_alpha_beta_u_S(self, R):
-		return self.E_alpha_beta_u_S(R) - self.scaled_M_alpha
+	# def tilde_E_alpha_beta_u_S(self, R):
+	# 	return self.E_alpha_beta_u_S(R) - self.scaled_M_alpha
 
 	def __call__(self, R):
-		return self.tilde_E_alpha_beta_u_S(R)
+		return self.E_alpha_u_S(R)
 
 if __name__ == "__main__":
 	import symmetry
@@ -111,9 +119,39 @@ if __name__ == "__main__":
 	print("Dim", out[0].shape)
 	print("Should be practically zero:", np.max(np.var(out, axis=0)))
 
+
+	print("\nDihedral group with 12 elements")
+	alpha = (2,3)
+	u = [np.array([1, 0, 0]), np.array([0, 1, 0])]
+	S = symmetry.DihedralGroup(6)
+	beta = (np.sqrt(5/12), np.sqrt(4)/3)
+	E = Embedding(alpha, u, S, beta)
+
+	R1 = np.eye(3)
+	orbit = S.orbit(R1)
+	out = [E(R) for R in orbit]
+	print("Dim", out[0].shape)
+	print("Should be practically zero:", np.max(np.var(out, axis=0)))
+
+
+	print("\nTetrahedral group")
+	alpha = (3,)
+	u = [np.array([0.5, 0.5, -0.5])]
+	S = symmetry.TetrahedralGroup()
+	beta = (3 / (2 * np.sqrt(2)),)
+	E = Embedding(alpha, u, S, beta)
+
+	R1 = np.eye(3)
+	orbit = S.orbit(R1)
+	out = [E(R) for R in orbit]
+	print("Dim", out[0].shape)
+	print("Should be practically zero:", np.max(np.var(out, axis=0)))
+
+
 	print("\nOctahedral group")
 	alpha = (4,)
 	u = [np.array([1, 0, 0])]
+	u[0] = u[0] / np.linalg.norm(u[0])
 	S = symmetry.OctahedralGroup()
 	beta = (3 / (2 * np.sqrt(2)),)
 	E = Embedding(alpha, u, S, beta)
@@ -123,17 +161,16 @@ if __name__ == "__main__":
 	out = [E(R) for R in orbit]
 	print("Dim", out[0].shape)
 	print("Should be practically zero:", np.max(np.var(out, axis=0)))
-	print("M_alpha", E.M_alpha)
 
-	print("\nIcosahedral group")
-	alpha = (10,)
-	u = [np.array([1, 0, 0])]
-	S = symmetry.IcosahedralGroup()
-	beta = (75 / (8 * np.sqrt(95)),)
-	E = Embedding(alpha, u, S, beta)
+	# print("\nIcosahedral group")
+	# alpha = (10,)
+	# u = [np.array([1, 0, 0])]
+	# S = symmetry.IcosahedralGroup()
+	# beta = (75 / (8 * np.sqrt(95)),)
+	# E = Embedding(alpha, u, S, beta)
 
-	R1 = np.eye(3)
-	orbit = S.orbit(R1)
-	out = [E(R) for R in orbit]
-	print("Dim", out[0].shape)
-	print("Should be practically zero:", np.max(np.var(out, axis=0)))
+	# R1 = np.eye(3)
+	# orbit = S.orbit(R1)
+	# out = [E(R) for R in orbit]
+	# print("Dim", out[0].shape)
+	# print("Should be practically zero:", np.max(np.var(out, axis=0)))
