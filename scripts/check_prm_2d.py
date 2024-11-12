447d32940750662b21ef5f9de99f53e3605a8437
cohnt <cohnt@umich.edu>
2024-11-12 13:41:19 -0500

    Test script for PRM 2d path planning. Verify that the build environment functionality works.

---
+++ scripts/check_prm_2d.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append("..")
+
+import numpy as np
+
+import src.planners.imacs as imacs
+import src.planners.prm as prm
+import src.symmetry as symmetry
+import src.worlds.path_planning_2d as path_planning_2d
+
+from pydrake.all import (
+    StartMeshcat
+)
+
+meshcat = StartMeshcat()
+
+limits = [[0, 20], [0, 20]]
+params = path_planning_2d.SetupParams(3, limits, 200, 1, 0)
+diagram, CollisionChecker = path_planning_2d.build_env(meshcat, params)
+
+G = symmetry.CyclicGroupSO2(3)
+Sampler = imacs.SO2SampleUniform(G, 3, 2, [limits[0][0], limits[1][0], 0], [limits[0][1], limits[1][1], 0])
+Metric = imacs.SO2DistanceSq(G, 3, 2)
+Interpolator = imacs.SO2Interpolate(G, 3, 2)
+options = prm.PRMOptions()
+roadmap = prm.PRM(Sampler, Metric, Interpolator, CollisionChecker, options)
+
+roadmap.build()
