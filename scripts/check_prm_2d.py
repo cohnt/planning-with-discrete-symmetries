Fix the scaling of connection k and radius for RRT* and PRM*.

scripts/check_prm_2d.py
@@ -30,6 +30,10 @@ c_free_volume *= limits[1][1] - limits[1][0]
 c_free_volume *= asymptotic.s1_volume()
 print("Symmetry-Aware PRM* Minimum Radius:", asymptotic.radius_prm(3, c_free_volume / 3))
 print("Symmetry-Unaware PRM* Minimum Radius:", asymptotic.radius_prm(3, c_free_volume))
+print("KNN-PRM* Minimum k:", asymptotic.knn_prm(3))
+
+options.neighbor_radius = asymptotic.radius_prm(3, c_free_volume)
+options.neighbor_k = asymptotic.knn_prm(3)
 
 G = symmetry.CyclicGroupSO2(3)
 Sampler = imacs.SO2SampleUniform(G, 3, 2, [limits[0][0], limits[1][0], 0], [limits[0][1], limits[1][1], 0])
@@ -38,7 +42,7 @@ Interpolator = imacs.SO2Interpolate(G, 3, 2)
 roadmap = prm.PRM(Sampler, Metric, Interpolator, CollisionChecker, options)
 
 np.random.seed(0)
-roadmap.build()
+roadmap.build(verbose=True)
 
 fname = "check_prm_2d_symmetric.pkl"
 roadmap.save(fname)
@@ -107,7 +111,7 @@ Interpolator = imacs.SO2Interpolate(G, 3, 2)
 roadmap = prm.PRM(Sampler, Metric, Interpolator, CollisionChecker, options)
 
 np.random.seed(0)
-roadmap.build()
+roadmap.build(verbose=True)
 
 fname = "check_prm_2d_baseline.pkl"
 roadmap.save(fname)
@@ -142,7 +146,8 @@ for t in np.linspace(traj2.start_time(), traj2.end_time(), 400):
 # Alternate visualizing each one
 
 while True:
-    for traj in [traj1, traj2]:
+    for traj, name in zip([traj1, traj2], ["Symmetry-Aware PRM*", "Symmetry-Unaware PRM*"]):
+        print(name)
         time.sleep(3)
         dt = traj.end_time() - traj.start_time()
         dt /= 400

scripts/check_rrt_star_2d.py
@@ -4,6 +4,7 @@ sys.path.append("..")
 import time
 import numpy as np
 
+import src.asymptotic_optimality_parameters as aop
 import src.planners.imacs as imacs
 import src.planners.rrt as rrt
 import src.planners.shortcut as shortcut
@@ -18,7 +19,7 @@ from pydrake.all import (
     Rgba
 )
 
-options = rrt.RRTOptions(max_vertices=750, max_iters=1e4, goal_sample_frequency=0.05, stop_at_goal=False)
+options = rrt.RRTOptions(max_vertices=1e3, max_iters=1e4, goal_sample_frequency=0.05, stop_at_goal=False, step_size=5.0)
 shortcut_options = shortcut.ShortcutOptions(max_iters=1e2)
 random_seed = 0
 
@@ -98,7 +99,15 @@ traj2 = CompositeTrajectory.AlignAndConcatenate(segments)
 
 # Check RRT*
 import src.planners.star as star
-rrt_star_options = star.RRTStarOptions(connection_radius=5.0, connection_k=12, mode="radius")
+
+c_space_dimension = 3
+c_space_volume = np.prod(np.array(limits)[:,1] - np.array(limits)[:,0]) * aop.s1_volume()
+rrt_star_radius_original = aop.radius_rrt(c_space_dimension, c_space_volume)
+rrt_star_radius_quotient = aop.radius_rrt(c_space_dimension, c_space_volume / G.order())
+rrt_star_options_even = star.RRTStarOptions(connection_radius=rrt_star_radius_original, mode="radius")
+rrt_star_options_uneven = star.RRTStarOptions(connection_radius=rrt_star_radius_quotient, mode="radius")
+
+rrt_star_options = star.RRTStarOptions(connection_radius=rrt_star_radius_original, connection_k=12, mode="radius")
 
 rrt_star = star.RRTStar(planner, rrt_star_options)
 new_path = rrt_star.return_plan()

src/asymptotic_optimality_parameters.py
@@ -1,16 +1,14 @@
 import numpy as np
 from scipy.special import gamma
 
-def knn_rrt(dimension, max_samples):
+def knn_rrt(dimension):
     k_opt = 2 ** (dimension + 1)
     k_opt *= np.e * (1 + (1 / dimension))
-    k_opt *= np.log(max_samples)
-    return int(np.ceil(k_opt))
+    return k_opt
 
-def knn_prm(dimension, max_samples):
+def knn_prm(dimension):
     k_opt = np.e * (1 + (1 / dimension))
-    k_opt *= np.log(max_samples)
-    return int(np.ceil(k_opt))
+    return k_opt
 
 def unit_ball_volume(dimension):
     return (np.pi ** (dimension / 2)) / gamma((dimension / 2) + 1)

src/planners/prm.py
@@ -8,12 +8,13 @@ import src.planners.imacs as imacs
 
 class PRMOptions:
     def __init__(self, neighbor_radius=1e-1, neighbor_k=12, neighbor_mode="k",
-                 check_size=1e-2, max_vertices=1e3):
+                 check_size=1e-2, max_vertices=1e3, scale=True):
         self.neighbor_radius = neighbor_radius
         self.neighbor_k = neighbor_k
         self.neighbor_mode = neighbor_mode # "radius", "k"
         self.check_size = check_size
         self.max_vertices = int(max_vertices)
+        self.scale = scale
 
 class PRM:
     def __init__(self, Sampler, Metric, Interpolator, CollisionChecker, options):
@@ -52,19 +53,30 @@ class PRM:
         dist_mat, targets = self.Metric.pairwise(nodes)
         np.fill_diagonal(dist_mat, np.inf)
 
+        dimension = self.Sampler.ambient_dim
+
         # Pick edges to check.
         edges_to_try = dict() # Keys will be tuples (i, j), values will be j_rep
         if self.options.neighbor_mode == "radius":
             for i in range(0, len(nodes)):
-                for j in range(i + 1, len(nodes)):
-                    if dist_mat[i,j] <= self.options.neighbor_radius:
+                card = i + 1
+                r = self.options.neighbor_radius
+                if self.options.scale:
+                    r *= (np.log(card) / card) ** (1/dimension)
+                for j in range(0, i - 1):
+                    if dist_mat[i,j] <= r:
                         edges_to_try.update({(i, j): targets[i, j]})
         elif self.options.neighbor_mode == "k":
             edge_counts = np.zeros(len(nodes), int)
             for i in range(len(nodes)):
-                j_list = np.argpartition(dist_mat[i], self.options.neighbor_k)[:self.options.neighbor_k]
+                card = i + 1
+                k = self.options.neighbor_k
+                if self.options.scale:
+                    k *= np.log(card)
+                k = int(np.ceil(k))
+                j_list = np.argpartition(dist_mat[i], k)[:k]
                 for j in j_list:
-                    if i < j:
+                    if i > j:
                         edges_to_try.update({(i, j): targets[i, j]})
                     else:
                         edges_to_try.update({(j, i): targets[j, i]})
@@ -73,7 +85,7 @@ class PRM:
 
         num_edges = len(edges_to_try)
         for (i, j), qj in tqdm(edges_to_try.items(), "Checking Edges for Collisions", disable=not verbose):
-            assert i < j
+            assert i > j
             self._maybe_connect(i, j, qj, dist=dist_mat[i,j])
 
         if verbose:
@@ -128,11 +140,20 @@ class PRM:
         dists, qis = self.Metric.pairwise(np.array([q]), all_qs)
         dists = dists.reshape(-1)
         qis = qis.reshape(-1, len(q))
+        card = len(self.graph)
+        dimension = self.Sampler.ambient_dim
         if self.options.neighbor_mode == "radius":
-            num_within_radius = np.sum(dists <= self.options.neighbor_radius)
+            r = self.options.neighbor_radius
+            if self.options.scale:
+                r *= (np.log(card) / card) ** (1/dimension)
+            num_within_radius = np.sum(dists <= r)
             idxs = np.argsort(dists)[:num_within_radius]
         elif self.options.neighbor_mode == "k":
-            idxs = np.argsort(dists)[:self.options.neighbor_k]
+            k = self.options.neighbor_k
+            if self.options.scale:
+                k *= np.log(card)
+            k = int(np.ceil(k))
+            idxs = np.argsort(dists)[:k]
         return idxs, qis[idxs]
 
     def _maybe_connect(self, i, j, qj, dist=None):

src/planners/star.py
@@ -6,10 +6,11 @@ import time
 import src.planners.rrt as rrt
 
 class RRTStarOptions:
-    def __init__(self, connection_radius=5.0, connection_k=12, mode="radius"):
+    def __init__(self, connection_radius=5.0, connection_k=12, mode="radius", scale=True):
         self.connection_radius = connection_radius
         self.connection_k = connection_k
         self.mode = mode
+        self.scale = scale
 
         assert isinstance(self.connection_k, int)
         assert self.connection_radius > 0
@@ -28,6 +29,8 @@ class RRTStar:
         dist_sq_mat, targets = self.rrt.Metric.pairwise(nodes)
         dist_mat = np.sqrt(dist_sq_mat)
 
+        dimension = self.rrt.Sampler.ambient_dim
+
         # Compute cost-to-come
         self._compute_cost_to_come()
 
@@ -37,12 +40,21 @@ class RRTStar:
         # Incrementally rewire
         for j in tqdm(range(1, len(self.rrt.tree)), desc="RRT* Rewiring", disable=not verbose):
             # Find neighbors
+            card = j + 1
             candidate_dists = dist_mat[j,:j]
             if self.options.mode == "k":
-                partitioned_indices = np.argpartition(candidate_dists, self.options.connection_k)
-                candidate_nodes = partitioned_indices[:self.options.connection_k]
+                k = self.options.connection_k
+                if self.options.scale:
+                    k *= np.log(card)
+                k = int(np.ceil(k))
+                partitioned_indices = np.argpartition(candidate_dists, k)
+                candidate_nodes = partitioned_indices[:k]
             elif self.options.mode == "radius":
-                candidate_nodes = np.where(candidate_dists <= self.options.connection_radius)[0]
+                r = self.options.connection_radius
+                if self.options.scale:
+                    r *= (np.log(card) / card) ** (1/dimension)
+                    r = min(r, self.rrt.options.step_size)
+                candidate_nodes = np.where(candidate_dists <= r)[0]
             else:
                 raise(NotImplementedError)
 