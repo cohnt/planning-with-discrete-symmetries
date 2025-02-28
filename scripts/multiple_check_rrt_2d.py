23f443d021401a3035c7b2d69dccaa82dd2ae0d8
cohnt <cohnt@umich.edu>
2025-02-27 21:29:57 -0500

    Video segment 1.

--- scripts/multiple_check_rrt_2d.py
+++ scripts/multiple_check_rrt_2d.py
@@ -21,11 +21,11 @@ shortcut_options = shortcut.ShortcutOptions(max_iters=1e3)
 
 meshcat = StartMeshcat()
 
-n_sides = 3
-# n_sides = 2
+# n_sides = 8
+n_sides = 2
 
 use_birrt = True
-n_copies = 5
+n_copies = 1
 # q0 = np.array([0.01, 0.01, 0,
 #                19.9, 0.01, 0,
 #                19.9, 19.9, 0,
@@ -56,11 +56,13 @@ else:
 shortcutter = shortcut.Shortcut(Metric, Interpolator, CollisionChecker, shortcut_options)
 
 qs = []
+np.random.seed(3)
 while len(qs) < 2:
     q = Sampler(1).flatten()
     if CollisionChecker.CheckConfigCollisionFree(q):
         qs.append(q)
 q0, q1 = qs
+print(q0, q1)
 
 diagram_context = diagram.CreateDefaultContext()
 plant_context = diagram.plant().GetMyContextFromRoot(diagram_context)
@@ -81,14 +83,14 @@ for i in range(1, len(path)):
 
 one_object_metric = imacs.SO2DistanceSq(G, 3, 2)
 print("SE(2)/G path length:", Metric.path_length(path))
-t_scaling = 1/4
+t_scaling = 2/4
 times = [t_scaling * np.sqrt(np.max([one_object_metric(path[i-1][j-2:j+1], path[i][j-2:j+1])[0] for j in symmetry_indices])) for i in range(1, len(path))]
 segments = [PiecewisePolynomial.FirstOrderHold([0, times[i-1]], np.array([path[i-1], path[i]]).T) for i in range(1, len(path))]
 traj1 = CompositeTrajectory.AlignAndConcatenate(segments)
 
-dt = traj1.end_time() - traj1.start_time()
-dt /= 400
-for t in np.linspace(traj1.start_time(), traj1.end_time(), 400):
+traj_dt = traj1.end_time() - traj1.start_time()
+dt = 1/60
+for t in np.linspace(traj1.start_time(), traj1.end_time(), int(traj_dt / dt)):
     diagram.plant().SetPositions(plant_context, traj1.value(t).flatten())
     diagram.ForcedPublish(diagram_context)
     time.sleep(dt)
@@ -119,14 +121,13 @@ for i in range(1, len(path)):
 
 one_object_metric = imacs.SO2DistanceSq(G, 3, 2)
 print("Baseline path length:", Metric.path_length(path))
-t_scaling = 1/4
 times = [t_scaling * np.sqrt(np.max([one_object_metric(path[i-1][j-2:j+1], path[i][j-2:j+1])[0] for j in symmetry_indices])) for i in range(1, len(path))]
 segments = [PiecewisePolynomial.FirstOrderHold([0, times[i-1]], np.array([path[i-1], path[i]]).T) for i in range(1, len(path))]
 traj2 = CompositeTrajectory.AlignAndConcatenate(segments)
 
-dt = traj1.end_time() - traj1.start_time()
-dt /= 400
-for t in np.linspace(traj2.start_time(), traj2.end_time(), 400):
+traj_dt = traj1.end_time() - traj1.start_time()
+dt = 1/60
+for t in np.linspace(traj2.start_time(), traj2.end_time(), int(traj_dt / dt)):
     diagram.plant().SetPositions(plant_context, traj2.value(t).flatten())
     diagram.ForcedPublish(diagram_context)
     time.sleep(dt)
@@ -136,10 +137,14 @@ for t in np.linspace(traj2.start_time(), traj2.end_time(), 400):
 while True:
     for traj, name in zip([traj1, traj2], ["symmetry aware", "symmetry unaware"]):
         time.sleep(3)
-        dt = traj.end_time() - traj.start_time()
-        dt /= 400
+        diagram.plant().SetPositions(plant_context, traj.value(traj.start_time()).flatten())
+        diagram.ForcedPublish(diagram_context)
+        time.sleep(3)
+
+        traj_dt = traj.end_time() - traj.start_time()
+        dt = 1/60
         print(name)
-        for t in np.linspace(traj.start_time(), traj.end_time(), 400):
+        for t in np.linspace(traj.start_time(), traj.end_time(), int(traj_dt / dt)):
             diagram.plant().SetPositions(plant_context, traj.value(t).flatten())
             diagram.ForcedPublish(diagram_context)
             time.sleep(dt)
